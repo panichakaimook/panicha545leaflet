b51b017e717719fa64a6409c04c5c25dd78b7b91
panichakaimook <124546134+panichakaimook@users.noreply.github.com>
2023-04-28 17:22:35 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดไทยาวาส",
+          "type": "culture",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.2264540000009,
+            13.791121594433335
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดไร่ขิง",
+          "type": "culture",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.2585860000014,
+            13.742125337567316
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ดอยอินทนนท์",
+          "type": "natural",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            98.48691849999915,
+            18.590755142405683
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกไทรโยคน้อย",
+          "type": "natural",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            99.0583340000046,
+            14.23787671889157
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "น้ำตกไทรโยคใหญ่",
+          "type": "natural",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            98.85232500000279,
+            14.435935312264576
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดบางพระ",
+          "type": "culture",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.05667699999861,
+            13.663632190757838
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนน้ำบึงกุ่ม",
+          "type": "special",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.6681339999995,
+            13.798764335548299
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอศิลป์",
+          "type": "special",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.55087500000104,
+            13.838951168905893
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สยามพารากอน",
+          "type": "special",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.53495599999928,
+            13.746692337404994
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะเสม็ด",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.5002907288382,
+            13.684959339598464
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
